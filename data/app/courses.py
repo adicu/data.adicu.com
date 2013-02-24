406d1b5336613f0aa2b1766d3b30db5b1006d69a
Justin Hines <jph2149@columbia.edu>
2013-02-24 12:26:49 -0500

    Add page limit to handler

--- data/app/courses.py
+++ data/app/courses.py
@@ -12,6 +12,8 @@ class CoursesHandler(app.basic.BaseHandler):
     def get(self):
         recognized_arguments = self.valid_query_arguments(model_functions)
         queries = self.get_recognized_arguments(recognized_arguments)
+        limit = self.get_int_argument("limit", 0)
+        page = self.get_int_argument("page", 0)
         if not queries:
             return self.error(status_code=400, status_txt="MISSING_QUERY_ARGUMENTS")
         self.pgquery.execute(queries, callback=self._finish)
