4360700af3f7a14a05f0c5730258c90af940989c
Justin Hines <jph2149@columbia.edu>
2013-02-28 17:00:48 -0500

    Fuzzy dining and multi params for menu items

--- data/models/dining/dining_functions.py
+++ data/models/dining/dining_functions.py
@@ -7,4 +7,5 @@ def meal_type(value):
     return "meal_type", re.compile(value, re.IGNORECASE)
 
 def menu_item(value):
-    return "menu", value
+    values = [re.compile(item, re.IGNORECASE) for item in value.split(",")]
+    return "menu", {"$all": values}
