5cfe6caa23aac75d1a58b3575bbbd55bb393cbe9
Justin Hines <jph2149@columbia.edu>
2013-03-01 15:38:25 -0500

    Add a small template for docs

--- data/app/main.py
+++ data/app/main.py
@@ -1,5 +1,22 @@
 import app.basic
 
 class MainHandler(app.basic.BaseHandler):
+    params = [
+            ("Documentation"),
+            ("Authentication"),
+            ("affairs/social_media"),
+            ("affairs/student_events"),
+            ("affairs/alumni_events"),
+            ("athletics"),
+            ("courses"),
+            ("dining"),
+            ("housing/room"),
+            ("housing/building"),
+            ("uem"),
+        ]
     def get(self):
-        self.render('index.html', message="Hello World!")
+        self.render('temp.html',
+                params=self.params,
+                current="Documentation",
+                queries = {"exampe":"example text"},
+                user=None,)
