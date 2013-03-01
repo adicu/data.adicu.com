8c4534039208dfe888675dd4081949db17c96aa7
Justin Hines <jph2149@columbia.edu>
2013-02-28 22:55:32 -0500

    Finish up uem

--- data/models/uem/uem_functions.py
+++ data/models/uem/uem_functions.py
@@ -6,3 +6,22 @@ def room(value):
 def event_name(value):
     return "Event Name", re.compile(value, re.IGNORECASE)
 
+def event_starts_before(value):
+    value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+    return "Start", {"$lt": value}
+
+def event_starts_after(value):
+    value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+    return "Start", {"$gt" : value}
+
+def event_ends_before(value):
+    value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+    return "End", {"$lt": value}
+
+def event_ends_after(value):
+    value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+    return "End", {"$gt": value}
+
+def event_on(vale):
+    value = datetime.strptime(value, "%Y-%m-%d")
+    return "Date", value
