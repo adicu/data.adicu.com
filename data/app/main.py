9f8a7a8cf5837687e52bc108fbb44abffca584f4
Justin Hines <jph2149@columbia.edu>
2013-03-01 17:56:45 -0500

    Add lead ins

--- data/app/main.py
+++ data/app/main.py
@@ -19,7 +19,9 @@ class MainHandler(app.basic.BaseHandler):
 class DocsHandler(app.basic.BaseHandler):
     pages = {
             "affairs" : {
-                "lead" : None,
+                "lead" : """The affairs endpoint is pretty different from the
+                others, as this one doesn't have any query parameters, just
+                ping the endpoint, and see all the results we have fly!""",
                 "endpoints" : {
                     "affairs/social_media" : {
                             "request": None,
@@ -40,7 +42,12 @@ class DocsHandler(app.basic.BaseHandler):
                 },
             },
             "athletics" : {
-                "lead" : None,
+                "lead" : """The athletics endpoint is kind of broken down by
+                feed -- results and schedules.  Play around with the different
+                sports, as some can be pretty interesting! After you've pinged
+                it a couple times you'll start to notice the differences in
+                format between results, and schedules and how sports names are
+                formatted""",
                 "endpoints" : {
                     "athletics" : {
                         "request": None,
@@ -50,7 +57,10 @@ class DocsHandler(app.basic.BaseHandler):
                 },
             },
             "courses" : {
-                "lead" : None,
+                "lead" : """This endpoint is hiding a whole bunch of data! Make
+                sure you use the different query parameters effectively to see
+                its all done behind.  Think of it as one giant sql table
+                you're making requests against.""",
                 "endpoints" : {
                     "courses" : {
                         "request" : None,
@@ -60,7 +70,10 @@ class DocsHandler(app.basic.BaseHandler):
                 },
             },
             "dining" : {
-                "lead" : None,
+                "lead" : """Dining, the must have endpoint for anyone with a
+                stomach on this campus! Check out the way locations are done,
+                they are abbreviated in a strange way, like 'fer' for
+                ferris.""",
                 "endpoints" :{
                     "dining" : {
                         "request" : "Request",
@@ -70,7 +83,11 @@ class DocsHandler(app.basic.BaseHandler):
                 },
             },
             "housing" : {
-                "lead" : None,
+                "lead" : """Housing is broken down into two different endpoints,
+                rooms and buildings.  Rooms has a bunch of room data (gasp) as
+                well as some lottery information... Buildings has a list of
+                amentities... We don't offer joins across these two datasets,
+                so get creative! ;)""",
                 "endpoints" : {
                     "housing/room" : {
                         "request" : None,
@@ -84,7 +101,11 @@ class DocsHandler(app.basic.BaseHandler):
                 },
             },
             "uem" : {
-                "lead" : None,
+                "lead" : """UEM, for University Events Management is a delayed
+                live feed of whats coming out UEM. Updated nightly, this
+                endpoint has all the confirmed booked space on campus that UEM
+                handles, which is alot.  Be sure to play around with the event
+                name query param, because its probably the richest one""",
                 "endpoints" : {
                     "uem" : {
                         "request" : None,
@@ -97,12 +118,24 @@ class DocsHandler(app.basic.BaseHandler):
     }
     main = {
          "Authentication" : {
-                "lead" : None,
+                "lead" : """To use any of the API's you must have an api_token
+                and appended to your string as api_token=TOKEN. To get a
+                token, login on the upper right. We use google auth to make
+                the auth request. Once signed in, visit the token page to see
+                your token, and get coding!""",
                 "endpoints" : None,
             },
 
             "Documentation"  : {
-                "lead" : "Hello World",
+                "lead" : """Documentation for the set of Data API's provided by,
+                and maintained by the Application Development Initative at
+                Columbia University.  Visit the different endpoints, get your
+                api token, and start coding. There is some exciting stuff
+                around there! If you have any set backs, feel free to ping
+                hack@adicu.com and we'll get back to you as quick as we can.
+                Please keep in mind this is API version 0.0 and is pretty
+                rough and the edges. Report bugs, and help us make it better
+                at github.com/adicu/data.adicu.com""",
                 "endpoints" : None,
             },
     }
