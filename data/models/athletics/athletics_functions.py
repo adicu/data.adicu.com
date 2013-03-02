b14ca670901e3144989a178f97d7af146d1df6e1
Justin Hines <jph2149@columbia.edu>
2013-03-01 19:41:35 -0500

    ath

--- data/models/athletics/athletics_functions.py
+++ data/models/athletics/athletics_functions.py
@@ -3,18 +3,22 @@ import athletics
 from datetime import datetime
 
 def sport(value):
+    """ """
     if value.lower() in athletics.sports():
         return "sport", value.lower()
     else:
         return "sport", re.compile(value, re.IGNORECASE)
 
 def feed(value):
+    """ """
     return "feed", re.compile(value, re.IGNORECASE)
 
 def title(value):
+    """ """
     return "title", re.compile(value, re.IGNORECASE)
 
 def win(value):
+    """ A boolean """
     if value.lower() == "true":
         value = True
     else:
@@ -23,15 +27,19 @@ def win(value):
     return "win", value
 
 def location(value):
+    """ """
     return "location", re.compile(value, re.IGNORECASE)
 
 def opponent(value):
+    """ """
     return "opponent", re.compile(value, re.IGNORECASE)
 
 def game_before(value):
+    """Format is Y-M-D H-M-S"""
     value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
     return "time", {"$lt": value}
 
 def game_after(value):
+    """Format is Y-M-D H-M-S"""
     value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
     return "time", {"$gt" : value}
