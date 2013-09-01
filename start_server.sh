39ec3d7988f56259d51a60441dddfe02728524b8
Howard Mao <zhehao.mao@gmail.com>
2013-09-01 15:00:39 -0400

    make sure start_server script kills child on SIGTERM

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-source ~/venv/bin/activate
+# make sure to catch the SIGTERM signal
+trap : SIGTERM
+
+# set up the environmental variables
+# supervisord will not do this for us
+USER=$(id -un)
+HOME=/home/$USER
+
+# start the python program in the background
+source $HOME/venv/bin/activate
 source config/settings.$1
-python data/data.py
+python data/data.py &
+FIND_PID=$!
+
+# wait for the program to finish
+wait $FIND_PID
+
+# If the return value from wait was greater than 128,
+# it means we caught a SIGTERM
+# kill off the python program in that case
+if [ $? -gt 128 ]; then
+	echo "Stopping data.adicu.com application server"
+	kill $FIND_PID
+fi
