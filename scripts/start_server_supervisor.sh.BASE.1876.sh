5b598dee712dbd35ffba24fa764ea3ae9f36a965
Parthiban Loganathan <pl2487@columbia.edu>
2014-04-13 19:28:58 -0400

    Revert elasticsearch changes

--- scripts/start_server_supervisor.sh.BASE.1876.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# set up the environmental variables
-# supervisord will not do this for us
-source $1
-python data/data.py # &
--- scripts/start_server_supervisor.sh.REMOTE.1876.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set up the environmental variables
-# supervisord will not do this for us
-source $1
-cd data
-python data.py # &
